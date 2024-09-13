2f5ede5b4a20eb3aacda8c5b8221fc5699a65c02
Andrew <vipklopa@mail.ru>
2024-09-13 14:57:35 +0300

    adding req column dateObs

--- astro/astro/settings.py
+++ astro/astro/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'astro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'astro/astro/templates'],
+        'DIRS': [BASE_DIR, 'astro/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
